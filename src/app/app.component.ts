36fe52200cb5ec635696ef373947c72e4066c251
chinmayachinu <chinmayapanigrahi.cp@gmail.com>
2021-04-06 16:29:03 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-template';
+  title = 'This is interpolation';
+  myBirthDay=new Date(1999,4,11);//since month parameter is zero based
+  myName:string='chinmaya'
+  toggleButton:boolean=true
+  isClassBinding:boolean=true
+  value:string=""
+  callPhone(phone:any){
+    alert(phone)
+  }
 }
